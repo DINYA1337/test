74853331c977c1976eb319a7666fdc5ae7e2dd7a
DINYA1337 <145918245+DINYA1337@users.noreply.github.com>
2023-09-24 18:50:21 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,31 @@
+# print("HI KIBERONE")
+# str1 = "22"
+# print(type(str1))
+# int1 = 2
+# print(type(int1))
+# float = 3.5
+# print(type(float))
+# c = [1,2,3,4]
+# print(type(1))
+
+# print(type(1+2.0))
+
+# print(type(2.4+1))
+
+# print(type(2.0 * 1))
+
+# John_age = 20
+# mom_age = 46
+# paul = (mom_age + John_age)/6
+# print(int(paul))
+
+# def imya_func(a,b):
+#     return 5 * x
+
+# print(my_function(3))
+# print(my_function(5))
+# print(my_function(9))
+
+
+
+#проверить есть ли повторяющиеся числа
